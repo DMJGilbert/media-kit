c7cc40ae85fe75ac1386d9bf38d16ee0bb7c421a
Sam Bowden <samuel.bowden@tutanota.com>
2025-09-18 15:46:17 +0100

    feat: add optional overlay widget to Video

--- media_kit_video/lib/src/video/video_texture.dart
+++ media_kit_video/lib/src/video/video_texture.dart
@@ -113,6 +113,8 @@ class Video extends StatefulWidget {
   /// The callback invoked when the [Video] exits fullscreen.
   final Future<void> Function() onExitFullscreen;
 
+  final Widget? overlay;
+
   /// {@macro video}
   const Video({
     Key? key,
@@ -131,6 +133,7 @@ class Video extends StatefulWidget {
     this.subtitleViewConfiguration = const SubtitleViewConfiguration(),
     this.onEnterFullscreen = defaultEnterNativeFullscreen,
     this.onExitFullscreen = defaultExitNativeFullscreen,
+    this.overlay,
   }) : super(key: key);
 
   @override
@@ -425,6 +428,8 @@ class VideoState extends State<Video> with WidgetsBindingObserver {
                                                       videoViewParameters.fill,
                                                 ),
                                               ),
+                                            if (widget.overlay != null)
+                                              widget.overlay!,
                                           ],
                                         ),
                                       );
